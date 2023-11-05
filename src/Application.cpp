6665ee70c0e4f2d27f2124e7f93ce6f9cb7c4ac9
tamighi <tamighi@hotmail.com>
2023-11-05 14:18:52 +0100

    Draw square with index buffers

--- src/Application.cpp
+++ src/Application.cpp
@@ -86,6 +86,18 @@ int main(void) {
     return -1;
   }
 
+  float positions[] = {
+    -0.5f, -0.5f,
+    0.5f, -0.5f,
+    0.5f, 0.5f,
+    -0.5f, 0.5f,
+  };
+
+  unsigned int indices[] = {
+    0, 1, 2,
+    2, 3, 0
+  };
+
   /* Buffer here */
   unsigned int buffer;
 
@@ -93,14 +105,24 @@ int main(void) {
   glGenBuffers(1, &buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
 
-  float positions[6] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f};
-
   /* Attribute data to the buffer */
-  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER,  6 * 2 * sizeof(float), positions,
+               GL_STATIC_DRAW);
 
   /* Describe the layout of the attributes in the vertex and enables it*/
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+  /* Buffer here */
+  unsigned int ibo;
+
+  /* Generate a buffer an select it */
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+  /* Attribute data to the buffer */
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices,
+               GL_STATIC_DRAW);
 
   /* Create shaders */
 
@@ -115,7 +137,7 @@ int main(void) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
